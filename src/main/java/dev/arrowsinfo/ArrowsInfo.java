2d8eb783236b5f66dab64451f4a10406a3d99e25
stlouisn <stlouisn@hotmail.com>
2024-09-08 20:17:04 -0300

    update deps

--- src/main/java/dev/arrowsinfo/ArrowsInfo.java
+++ src/main/java/dev/arrowsinfo/ArrowsInfo.java
@@ -4,9 +4,9 @@ import net.fabricmc.api.ClientModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@SuppressWarnings("unused")
-public class ArrowsInfo implements ClientModInitializer {
+public final class ArrowsInfo implements ClientModInitializer {
 
+  @SuppressWarnings("unused")
   public static final Logger LOGGER = LoggerFactory.getLogger(Constants.MOD_ID);
 
   public void onInitializeClient() {
--- src/main/java/dev/arrowsinfo/Constants.java
+++ src/main/java/dev/arrowsinfo/Constants.java
@@ -1,10 +1,11 @@
 package dev.arrowsinfo;
 
-public class Constants {
+public final class Constants {
 
   public static final String MOD_ID = "arrowsinfo";
 
   public static Placement placement = Placement.OFFHAND;
 
   public enum Placement {OFFHAND, MAINHAND}
+
 }
