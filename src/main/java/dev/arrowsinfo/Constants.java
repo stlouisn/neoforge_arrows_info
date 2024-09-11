89e3b598cb24b3bff2fea7ef748339e0de6475aa
stlouisn <stlouisn@hotmail.com>
2024-09-11 10:30:48 -0300

    Update Constants.java

--- src/main/java/dev/arrowsinfo/Constants.java
+++ src/main/java/dev/arrowsinfo/Constants.java
@@ -5,7 +5,5 @@ public final class Constants {
   public static final String MOD_ID = "arrowsinfo";
 
   public static Placement placement = Placement.OFFHAND;
-
   public enum Placement {OFFHAND, MAINHAND}
-
 }
